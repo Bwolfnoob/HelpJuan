09afec3110b9bafded9fa5010a784a4325de27b8
Vilson Dauinheimer <bwolfnoob@gmail.com>
2018-04-12 22:15:32 -0300

    Adicionado teste para o Diego Vieira

---
+++ Coffsy.WebClient/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceReference1;
+
+namespace Coffsy.WebClient
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var a = new AtendeClienteClient();
+            var cliente = a.buscaClienteAsync("" , "","","");
+        }
+    }
+}
